a86e6a7d1dce30de367844f9c876239f0ac84e01
PierreEdFils <epierrefils@gmail.com>
2022-08-06 17:07:41 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/covidtrackapi/CovidtrackapiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.covidtrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidtrackapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidtrackapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/covidtrackapi/CovidtrackapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.covidtrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidtrackapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
